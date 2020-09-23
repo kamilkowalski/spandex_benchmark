8c2b4d12c9eb32d7f9cba30d681066df0e34572b
Kamil Kowalski <kamil@kamilkowalski.pl>
2020-09-23 18:37:12 +0200

    Initialize mix project

---
+++ lib/spandex_performance_test.ex
@@ -0,0 +1,18 @@
+defmodule SpandexPerformanceTest do
+  @moduledoc """
+  Documentation for `SpandexPerformanceTest`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> SpandexPerformanceTest.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/spandex_performance_test_test.exs
@@ -0,0 +1,8 @@
+defmodule SpandexPerformanceTestTest do
+  use ExUnit.Case
+  doctest SpandexPerformanceTest
+
+  test "greets the world" do
+    assert SpandexPerformanceTest.hello() == :world
+  end
+end
